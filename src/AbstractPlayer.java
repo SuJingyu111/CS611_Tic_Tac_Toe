274a882f5a6210769996807bf613b18492e114f4
SuJingyu111 <gar123su@gmail.com>
2021-09-19 16:54:27 -0400

    AbstractPlayer And TicTacToePlayer

--- src/AbstractPlayer.java
+++ src/AbstractPlayer.java
@@ -1,2 +1,27 @@
-public class AbstractPlayer {
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractPlayer {
+
+    protected String name;
+
+    protected AbstractBoard board;
+
+    protected Map<String, AbstractPiece> pieces;
+
+    public AbstractPlayer(String name, AbstractBoard board) {
+        this.name = name;
+        this.board = board;
+        pieces = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void put(int row, int col, String pieceName);
+
+    public abstract boolean isWinner();
+
+
 }
--- src/TicTacToePlayer.java
+++ src/TicTacToePlayer.java
@@ -1,8 +1,4 @@
-class Player {
-
-    private String name;
-
-    private TicTacToeBoard board;
+class TicTacToePlayer extends AbstractPlayer{
 
     private int lastMoveRow, lastMoveCol;
 
@@ -10,31 +6,27 @@ class Player {
 
     private final int[][] directions;
 
-    public Player(String name, TicTacToeBoard board) {
-        this.name = name;
-        this.board = board;
+    public TicTacToePlayer(String name, TicTacToeBoard board) {
+        super(name, board);
         lastMoveRow = -1;
         lastMoveCol = -1;
         winCnt = 0;
         directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
-    }
-
-    public String getName() {
-        return name;
+        pieces.put(name, new TicTacToePiece(name));
     }
 
     public int getWinCnt() { return winCnt; };
 
-    public void put(int row, int col) {
+    public void put(int row, int col, String pieceName) {
         lastMoveRow = row;
         lastMoveCol = col;
-        board.put(row, col, this.getName());
+        board.put(row, col, pieces.get(pieceName));
     }
 
     public boolean isWinner() {
-        int[] parameters = board.getParameters();
+        int[] parameters = ((TicTacToeBoard)board).getParameters();
         int r = parameters[0], c = parameters[1];
-        int step1 = 0, step2 = 0, winningCriterion = board.getWinningCriterion();
+        int step1 = 0, step2 = 0, winningCriterion = ((TicTacToeBoard)board).getWinningCriterion();
         for (int i = 0; i < 4; i++) {
             int[] direction1 = directions[2 * i], direction2 = directions[2 * i + 1];
             step1 = getLength(direction1, r, c);
@@ -51,7 +43,7 @@ class Player {
         int step = 0;
         while (lastMoveRow + (step + 1) * direction[0] < r && lastMoveRow + (step + 1) * direction[0] >= 0
                 && lastMoveCol + (step + 1) * direction[1] < c && lastMoveCol + (step + 1) * direction[1] >= 0) {
-            if (board.getPositionString(lastMoveRow + (step + 1) * direction[0], lastMoveCol + (step + 1) * direction[1]).equals(name)) {
+            if (board.getPieceName(lastMoveRow + (step + 1) * direction[0], lastMoveCol + (step + 1) * direction[1]).equals(name)) {
                 step++;
             }
             else break;
