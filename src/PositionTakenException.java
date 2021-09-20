636dfbc28890895ea2fb549af097ede515c8cf55
SuJingyu111 <gar123su@gmail.com>
2021-09-20 19:03:19 -0400

    Implemented PositionTakenException class

---
+++ src/PositionTakenException.java
@@ -0,0 +1,11 @@
+public class PositionTakenException extends RuntimeException{
+
+    public PositionTakenException() {
+        super();
+    }
+
+    public PositionTakenException(String message) {
+        super(message);
+    }
+
+}
